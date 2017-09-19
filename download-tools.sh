181e207ae591ffa109637cbb7bb31edff361f4ce
Aesen Vismea <aesen@unascribed.com>
2017-09-19 06:42:59 -0400

    Use a patched CMPDL which won't try to open dialogs

--- download-tools.sh
+++ download-tools.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -L https://github.com/Vazkii/CMPDL/releases/download/1.1.3/cmpdl-1.1.3.jar -o cmpdl.jar
+curl -L https://unascribed.com/f/3669771a_cmpdl-hacked.jar -o cmpdl.jar
 curl -L https://unascribed.com/f/ed9ba18b_Modalyzer-700438a-hacked.jar -o modalyzer.jar
