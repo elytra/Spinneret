73f47cad1d47fa21397cdaa0384787b7e5ded355
Aesen Vismea <aesen@unascribed.com>
2017-09-19 06:57:27 -0400

    Don't fail when the working directory has spaces, add make-zip.sh

--- download-curse-modpack.sh
+++ download-curse-modpack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -jar cmpdl.jar "https://minecraft.curseforge.com/projects/$1" latest | tee cmpdl.log
 out=`grep 'Output Path: ' cmpdl.log | tail -n 1 | sed 's/Output Path: //'`
-mv $out modpack
+mv "$out" modpack
---
+++ make-zip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir out
+cd modpack/minecraft
+zip -r9 ../../out/$JOB_BASE_NAME.zip *
